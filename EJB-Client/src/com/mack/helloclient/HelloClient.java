97977eee063f14a94ecba003d9a54495d030c3da
William <hikawa.kun@gmail.com>
2013-03-31 03:20:31 -0300

    Classes Servidor EJB Terminados

---
+++ EJB-Client/src/com/mack/helloclient/HelloClient.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mack.helloclient;
+import javax.naming.*;
+import com.mack.meuPacote.SimpleBean; 
+import java.util.Properties;
+/**
+ *
+ * @author H_W
+ */
+public class HelloClient {
+   public void runTest() throws Exception { 
+      Properties props = new Properties();
+      props.load(new java.io.FileInputStream("jndi.properties"));
+
+      InitialContext ctx = new InitialContext(props);
+      SimpleBean bean = (SimpleBean) ctx.lookup("ejb/Hello");
+      String result = bean.sayHello("Bom dia!");
+      System.out.println(result);
+   }
+
+   public static void main(String[] args) {
+      try {
+         HelloClient cli = new HelloClient();
+         cli.runTest();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+}
