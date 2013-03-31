f9802b7fe59aa53900817a05a897841bfbd78396
William <hikawa.kun@gmail.com>
2013-03-31 04:05:19 -0300

    Modificacoes para imprimir data + hora do servidor

--- EJB-Server/src/java/com/mack/meuPacote/HelloBean.java
+++ EJB-Server/src/java/com/mack/meuPacote/HelloBean.java
@@ -4,16 +4,30 @@
  */
 package com.mack.meuPacote;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.ejb.Stateless;
 
 /**
  *
  * @author H_W
  */
-@Stateless (mappedName="ejb/Hello") 
-public class HelloBean implements SimpleBean { 
-   @Override 
-   public String sayHello(String name) { 
-      return "Hello " + name + "!"; 
-   } 
+@Stateless(mappedName = "ejb/Hello")
+public class HelloBean implements SimpleBean {
+    /*
+     * Implementacao antiga - retornava só 'Hello' antes do nome e '!' depois do nome tudo concatenado em uma string
+     @Override 
+     public String sayHello(String name) { 
+     return "Hello " + name + "!"; 
+     } 
+     * 
+     * Implementacao nova - Retorna dia/mes/ano hora:min:seg do servidor em uma unica string
+     */
+    @Override
+    public String sayHello(String name) {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date).toString();
+    }
 }
